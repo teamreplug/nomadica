9e0982d53678bf404021fef2f6149350447c9f03
Adeyemo Gabriel <adeyemogab@gmail.com>
2016-04-21 22:42:40 +0100

    I prepared the way for some plugins, added a controller handle addFeed.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,58 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngStorage'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+//   ////uncomment from here to 
+//     //Notification template
+
+//   //a function to be used in the request
+//   $scope.refresh_location = function(){
+//     var posOptions = {timeout: 10000, enableHighAccuracy: false};
+//   $cordovaGeolocation
+//     .getCurrentPosition(posOptions)
+//     .then(function (position) {
+//       var lat  = position.coords.latitude
+//       var long = position.coords.longitude
+//       Data.post('API_URL', Json_Format to be Posted).then(function(response){
+//         //do something
+//       })
+//     }, function(err) {
+//       // error
+//     });
+//   }
+//   //to check location
+//     var posOptions = {timeout: 10000, enableHighAccuracy: false};
+//   $cordovaGeolocation
+//     .getCurrentPosition(posOptions)
+//     .then(function (position) {
+//       var lat  = position.coords.latitude
+//       var long = position.coords.longitude
+//       Data.post('API_URL', Json_Format to be Posted).then(function(response){
+//         //do something
+//       })
+//     }, function(err) {
+//       // error
+//     });
+//     ///to watch for location every  1 hour
+//         var watchOptions = {
+//         timeout : 3600000,
+//         enableHighAccuracy: false // may cause errors if true
+//       };
+
+//       var watch = $cordovaGeolocation.watchPosition(watchOptions);
+//       watch.then(
+//         null,
+//         function(err) {
+//           // error
+//         },
+//         function(position) {
+//           var lat  = position.coords.latitude
+//           var long = position.coords.longitude
+//           Data.post('API_URL', Json_Format to be Posted).then(function(response){
+//         //do something
+//       })
+//       });
+//       ///Stop uncommenting
+// //////////
   // Form data for the login modal
   $scope.loginData = {};
 
@@ -45,19 +97,33 @@ angular.module('starter.controllers', [])
 
 /* Replug Controller */
 
-.controller('SetupController', function($rootScope,$scope,Data,$ionicLoading,$state){
+.controller('SetupController', function($rootScope,$scope,Data,$ionicLoading,$state,$cordovaDevice){
+  $scope.showAlert = function(msg) {
+     var alertPopup = $ionicPopup.alert({
+     title: 'Info',
+     template: msg
+     });
+   };
 
   $scope.submitSetup = function (userDetails){
 
     $ionicLoading.show();
+     document.addEventListener("deviceready", function () {
 
-    Data.post('setUp', {"name": userDetails.name, "phone": userDetails.phone, "device_type": "Android", "device_version": "4.3","device_imei":"6oidiudiu78732"})
+    $scope.platform = $cordovaDevice.getPlatform();
+
+    $scope.uuid = $cordovaDevice.getUUID();
+
+    $socpe.version = $cordovaDevice.getVersion();
+
+  }, false);
+    Data.post('setUp', {"name": userDetails.name, "phone": userDetails.phone, "device_type":$scope.platform , "device_version": $scope.version,"device_imei":$scope.uuid})
     .then(function(response){
 
       $ionicLoading.hide();
       if(response.status === "success" || response.status === "info"){
         //local store the response
-        // $localStorage.whateverpack = response;
+        $localStorage.User = response;
         $rootScope.userId = response._id;
         $state.go("app.main");
       }else{
@@ -67,8 +133,11 @@ angular.module('starter.controllers', [])
       }
 
     }, function(error){
+
         //popup
+
         $scope.authMsg = "Sorry! an unknown error occured, please try later";
+        $scope.showAlert($scope.authMsg);
 
         console.log(error);
 
@@ -177,3 +246,22 @@ angular.module('starter.controllers', [])
   
   $scope.reload()
 });
+//controller for feeds
+// .controller('FeedController', function($scope,$rootScope,Data){
+//   $scope.submit = function(FeedDetails){
+//     $ionicLoading.show();
+//     Data.post('addFeed', {
+//       "user_id":$localStorage.user_id,
+//       "title": FeedDetails.title,
+//       "content": FeedDetails.content,
+//       "location_lon":$localStorage.current.lon,
+//       "location_lat":$localStorage.current.lat,
+//       "location_area":$localStorage.current.area
+//     }).then(function(response){
+//       $ionicLoading.hide();
+//       if(response.message === "success"){
+//         //do something.
+//       }
+//     })
+//   }
+// })
